8134761cc6eb908b5fab1909da7218bdf16ef1f9
DHToBB <1021287191@qq.com>
2021-11-02 13:46:47 +0800

    删除无用代码

--- practice/p_1.1/main.go
+++ practice/p_1.1/main.go
@@ -2,29 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
-
-func producer(ch chan<- int) {
-	for i := 0; i < 20; i++ {
-		time.Sleep(1 * time.Second)
-		ch <- i
-		fmt.Println("producer: ", i)
-	}
-	fmt.Println("stop to send data")
-}
-
-func consumer(ch <-chan int) {
-	time.Sleep(20 * time.Second)
-	fmt.Println("consumer begin to receive data")
-
-	for {
-		select {
-		case data := <-ch:
-			fmt.Println("consumer: ", data)
-		}
-	}
-}
 
 func main() {
 	arr := []string{"I", "am", "stupid", "and", "weak"}
